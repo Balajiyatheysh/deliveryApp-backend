a2e4f64c46d6a501fb115530a46c6100a23a9ab3
balajiyatheysh <balajiyatheysh87@gmail.com>
2023-12-28 23:25:52 +0530

    28-12-2023

--- index.ts
+++ index.ts
@@ -1,42 +1,13 @@
-import express, {Request, Response, NextFunction} from 'express'
-import { AdminRoute, VendorRoute } from './routes'
-import mongoose from 'mongoose'; 
-import dotenv from 'dotenv'
-import path from 'path'
+import express from 'express';
+import App from './services/ExpressApp';
+import databaseConnection  from './services/Database';
 
-(async () => {
-  // Load the environment variables
-  await dotenv.config({
-    path: './.env'
-  });
-})();
-
-
-
-const app = express()
-
-app.use(express.json());
-app.use(express.urlencoded({extended: true}))
-app.use('/images' , express.static(path.join(__filename, 'images')))
-
-const connectDB = async ()=>{
-  try {
-    const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}`)
-    console.log(`\nMongoDB connected !! DB HOST: ${connectionInstance.connection.host} `);
-  } catch (error) {
-    console.log("mongodb connection failed at db/index.js", error);
-    process.exit(1);
-  }
+const StartServer = async () =>{
+  const app = express();
+  await databaseConnection();
+  await App(app);
+  app.listen(8000, ()=>{
+    console.log(`Listening to port number 8000`)
+  })
 }
-
-connectDB()
-        
-const PORT =8000
-
-app.use("/admin", AdminRoute);
-app.use("/vendor", VendorRoute);
-
-app.listen(PORT, ()=>{
-  console.clear()
-  console.log(`Listening to port number ${PORT}`)
-})
+StartServer();
---
+++ services/ExpressApp.ts
@@ -0,0 +1,17 @@
+import express, { Application} from 'express'
+import { AdminRoute, VendorRoute } from '../routes'
+import path from 'path'
+
+export default async(app: Application)=>{
+
+  app.use(express.json());
+  app.use(express.urlencoded({extended: true}))
+  app.use('/images' , express.static(path.join(__filename, 'images')))
+    
+  app.use("/admin", AdminRoute);
+  app.use("/vendor", VendorRoute);
+  
+  return app
+}
+
+
